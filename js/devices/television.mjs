024da1135d1cc571c1bf9ec747afe1efd7e243b9
TetyanaKopot <kopot.tetyana@gmail.com>
2024-08-09 21:13:19 +0300

    Class Television

---
+++ js/devices/television.mjs
@@ -0,0 +1,38 @@
+import { Device } from '../app.mjs'
+
+export class Television extends Device {
+  constructor(name, channels) {
+    super(name)
+    this.channels = channels
+    this.currentChannel = null
+    this.volume = 10
+  }
+  switchChannel(channelNumber) {
+    if (
+      this.isOn &&
+      channelNumber > 0 &&
+      channelNumber < this.channels.length
+    ) {
+      this.currentChannel = channels[channelNumber]
+      console.log(`Switch to channel ${this.currentChannel}`)
+    } else {
+      console.log('Invalid channel or TV is OFF')
+    }
+  }
+
+  adjustVolume(volumeLevel) {
+    if (this.isOn) {
+      this.volume = Math.min(Math.max(volumeLevel, 0), 100)
+      console.log(`Volume set to ${this.volume}`)
+    } else {
+      console.log('Cannot adjust volume. TV is OFF')
+    }
+  }
+
+  getChannelList() {
+    return this.channels
+  }
+}
+
+const channels = ['BBC', 'CNN', 'National Geographic', 'Discovery']
+const myTV = new Television('Living-room TV', channels)
